8c3ae503dd0f5ef90be4ea6f9c0411226ff65f9c
shihao <coder403108@gmail.com>
2020-03-13 19:34:22 +0700

    create splash screen

--- www/js/index.js
+++ www/js/index.js
@@ -40,6 +40,7 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+        window.alert("xin chào các bạn");
     }
 };
 
